a63c28e0e15106744bd63417915e2401854fbcd3
Azim85 <avaz_53@icloud.com>
2021-09-11 18:24:33 +0500

    product model added

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CategoryModel, ProductModel
 
-# Register your models here.
+admin.site.register(CategoryModel)
+admin.site.register(ProductModel)
+
