b2426ce3dc523351878695f0946746f641970cc4
Sardor <sardor.mahkamov0701@gmail.com>
2021-09-11 16:30:37 +0500

    sign up

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import user_login, user_logout
+from user.views import user_login, user_logout, UserRegistration
 
 app_name = "user"
 
 urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('sign_up/', UserRegistration.as_view(), name="sign_up")
 ]
